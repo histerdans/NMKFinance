3d66ced2db2635e960c270ac5974cc86cd873a98
Dandee <daniel.ktanui14@gmail.com>
2021-03-30 00:12:24 +0300

    update new gj-new

--- GeneralLedger/forms.py
+++ GeneralLedger/forms.py
@@ -8,32 +8,33 @@ class FormNewGL(ModelForm):
         model = GeneralLedger
         fields = (
             'slug_number', 'item_name', 'description', 'partner', 'invoice', 'transaction_ref_number', 'account_name',
-            'account_number', 'debit', 'credit','tax_amount', 'paye_amt', 'invoice_amount',)
+            'account_number', 'debit', 'credit', 'date_due', 'tax_amount', 'paye_amt', 'invoice_amount',)
 
-        def __init__(self, *args, **kwargs):
-            super(RegisterForm, self).__init__(*args, **kwargs)
-            self.fields["item_name"].widget.attrs.update(
-                {'type': 'text', 'class': 'span12', 'id': 'item_name', 'name': 'item_name', })
-            self.fields['description'].widget.attrs.update(
-                {'type': 'text', 'class': 'span12', 'id': 'description', 'name': 'description', })
-            self.fields['partner'].widget.attrs.update(
-                {'type': 'email', 'class': 'span12', 'id': 'partner', 'name': 'partner', })
-            self.fields['invoice'].widget.attrs.update(
-                {'type': 'text', 'class': 'span12', 'id': 'invoice', 'name': 'invoice',})
-            self.fields['transaction_ref_number'].widget.attrs.update(
-                {'type': 'text', 'class': 'span12', 'id': 'transaction_ref_number', 'name': 'transaction_ref_number', })
-            self.fields['account_name'].widget.attrs.update(
-                {'type': 'text', 'class': 'span12', 'id': 'account_name', 'name': 'account_name', })
-            self.fields['account_number'].widget.attrs.update(
-                {'type': 'text', 'class': 'span12', 'id': 'account_number', 'name': 'account_number'})
-            self.fields['debit'].widget.attrs.update(
-                {'type': 'text', 'class': 'span12', 'id': 'debit', 'name': 'debit'})
-            self.fields['credit'].widget.attrs.update(
-                {'type': 'text', 'class': 'span12', 'id': 'credit', 'name': 'credit',})
-            self.fields['tax_amount'].widget.attrs.update(
-                {'type': 'text', 'class': 'span12', 'id': 'tax_amount', 'name': 'tax_amount', })
-            self.fields['paye_amt'].widget.attrs.update(
-                {'type': 'text', 'class': 'span12', 'id': 'paye_amt', 'name': 'paye_amt', })
-            self.fields['invoice_amount'].widget.attrs.update(
-                {'type': 'text', 'class': 'span12', 'id': 'invoice_amount', 'name': 'invoice_amount'})
-
+    def __init__(self, *args, **kwargs):
+        super(FormNewGL, self).__init__(*args, **kwargs)
+        self.fields["item_name"].widget.attrs.update(
+            {'type': 'text', 'class': 'span10', 'id': 'item_name', 'name': 'item_name', })
+        self.fields['description'].widget.attrs.update(
+            {'type': 'text', 'class': 'span8', 'id': 'description', 'name': 'description', })
+        self.fields['partner'].widget.attrs.update(
+            {'type': 'email', 'class': 'span8', 'id': 'partner', 'name': 'partner', })
+        self.fields['invoice'].widget.attrs.update(
+            {'type': 'text', 'class': 'span6', 'id': 'invoice', 'name': 'invoice', })
+        self.fields['transaction_ref_number'].widget.attrs.update(
+            {'type': 'text', 'class': 'span6', 'id': 'transaction_ref_number', 'name': 'transaction_ref_number', })
+        self.fields['account_name'].widget.attrs.update(
+            {'type': 'text', 'class': 'span10', 'id': 'account_name', 'name': 'account_name', })
+        self.fields['account_number'].widget.attrs.update(
+            {'type': 'text', 'class': 'span6', 'id': 'account_number', 'name': 'account_number'})
+        self.fields['debit'].widget.attrs.update(
+            {'type': 'text', 'class': 'span4', 'id': 'debit', 'name': 'debit'})
+        self.fields['credit'].widget.attrs.update(
+            {'type': 'text', 'class': 'span4', 'id': 'credit', 'name': 'credit', })
+        self.fields['date_due'].widget.attrs.update(
+            {'type': 'text', 'class': 'span4', 'id': 'date_due', 'name': 'date_due', })
+        self.fields['tax_amount'].widget.attrs.update(
+            {'type': 'text', 'class': 'span4', 'id': 'tax_amount', 'name': 'tax_amount', })
+        self.fields['paye_amt'].widget.attrs.update(
+            {'type': 'text', 'class': 'span4', 'id': 'paye_amt', 'name': 'paye_amt', })
+        self.fields['invoice_amount'].widget.attrs.update(
+            {'type': 'text', 'class': 'span4', 'id': 'invoice_amount', 'name': 'invoice_amount'})
--- GeneralLedger/views.py
+++ GeneralLedger/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import render
@@ -14,34 +15,14 @@ def add_new_GL(request):
 
         form = FormNewGL(request.POST)
         if form.is_valid():
-            slug_number = request.POST.get('slug_number', '')
-            item_name = request.POST.get('item_name', '')
-            description = request.POST.get('description', '')
-            partner = request.POST.get('partner', '')
-            invoice = request.POST.get('invoice', '')
-            invoice_amount = request.POST.get('invoice_amount', '')
-            transaction_ref_number = request.POST.get('transaction_ref_number', '')
-            account_name = request.POST.get('account_name', '')
-            account_number = request.POST.get('account_number', '')
-            debit = request.POST.get('debit', '')
-            credit = request.POST.get('credit', '')
-            paye_amt = request.POST.get('paye_amt', '')
-            user_obj = GeneralLedger(slug_number=slug_number,
-                                     item_name=item_name,
-                                     description=description,
-                                     partner=partner,
-                                     invoice=invoice,
-                                     invoice_amount=invoice_amount,
-                                     transaction_ref_number=transaction_ref_number,
-                                     account_name=account_name,
-                                     account_number=account_number,
-                                     debit=debit,
-                                     credit=credit,
-                                     paye_amt=paye_amt, )
-            user_obj.save()
+            form.save()
+            messages.success(request, 'GL ADDED')
             print('SUCCESS GL ADDED')
+        context = {'form': form}
+        return render(request, template_name, context)
     else:
-        return render(request, template_name, {'form': form, })
+        context = {'form': form, }
+        return render(request, template_name, context)
 
 
 @login_required(login_url='accounts:login')

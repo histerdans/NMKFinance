24ba12116bbcc27cc5081f328091d692b8dea57d
histerdans <daniel.ktanui14@gmail.com>
2021-11-10 10:59:30 +0300

    update new files 10/11/2021 requirements.txt

--- Finance/settings.py
+++ Finance/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', 'nmkfinance.herokuapp.com/']
 
 # Application definition
-
+DISABLE_COLLECTSTATIC=1
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',

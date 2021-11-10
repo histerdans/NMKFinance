57bde9039c40edf22438f195b0a301c82736c150
histerdans <daniel.ktanui14@gmail.com>
2021-11-10 11:19:45 +0300

    update new files 10/11/2021 requirements.txt

--- Finance/settings.py
+++ Finance/settings.py
@@ -126,13 +126,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
+# staticfiles files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
